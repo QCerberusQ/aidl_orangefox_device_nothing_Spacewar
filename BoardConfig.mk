08b694cd1438a672600b33ecc71f04274a1856c2
QCerberusQ <113352008+QCerberusQ@users.noreply.github.com>
2026-03-17 14:32:08 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,6 @@
+# -----------------------------------------------------------------------------
 # Broken rules
+# -----------------------------------------------------------------------------
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 ALLOW_MISSING_DEPENDENCIES := true
@@ -7,10 +9,14 @@ BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 
 DEVICE_PATH := device/nothing/Spacewar
 
+# -----------------------------------------------------------------------------
 # Platform
+# -----------------------------------------------------------------------------
 TARGET_BOARD_PLATFORM := lahaina
 
+# -----------------------------------------------------------------------------
 # A/B
+# -----------------------------------------------------------------------------
 AB_OTA_UPDATER := true
 
 AB_OTA_PARTITIONS += \
@@ -26,7 +32,9 @@ AB_OTA_PARTITIONS += \
     vendor_dlkm \
     vendor_boot
 
+# -----------------------------------------------------------------------------
 # Architecture
+# -----------------------------------------------------------------------------
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
@@ -47,12 +55,16 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_USES_64_BIT_BINDER := true
 TARGET_SUPPORTS_64_BIT_APPS := true
 
+# -----------------------------------------------------------------------------
 # Bootloader
+# -----------------------------------------------------------------------------
 TARGET_BOOTLOADER_BOARD_NAME := Spacewar
 TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
 
+# -----------------------------------------------------------------------------
 # Partitions
+# -----------------------------------------------------------------------------
 BOARD_FLASH_BLOCK_SIZE := 262144
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := $(BOARD_BOOTIMAGE_PARTITION_SIZE)
@@ -62,7 +74,9 @@ BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
 BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system system_ext vendor vendor_dlkm
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 6438256640
 
+# -----------------------------------------------------------------------------
 # Kernel
+# -----------------------------------------------------------------------------
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_NO_KERNEL := false
 TARGET_KERNEL_ARCH := arm64
@@ -98,7 +112,9 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 
+# -----------------------------------------------------------------------------
 # File systems
+# -----------------------------------------------------------------------------
 BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := erofs
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
@@ -116,48 +132,43 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_RAMDISK_USE_LZ4 := true
 
-#Vendor DLKM
+# -----------------------------------------------------------------------------
+# Vendor DLKM
+# -----------------------------------------------------------------------------
 BOARD_USES_VENDOR_DLKMIMAGE := true
 TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
 TARGET_COPY_OUT_VENDOR_BOOT := vendor_boot
 
+# -----------------------------------------------------------------------------
 # vAB Compress
+# -----------------------------------------------------------------------------
 BOARD_VIRTUAL_AB_COMPRESSION := true
+BOARD_VIRTUAL_AB_COMPRESSION_METHOD := none
 
 # -----------------------------------------------------------------------------
-# Kernel & Boot Image Offsets (v3 Fixed)
-# -----------------------------------------------------------------------------
-# Standart Qualcomm Offsetleri (Spacewar için genelde budur)
-BOARD_KERNEL_BASE        := 0x00000000
-BOARD_KERNEL_OFFSET      := 0x00008000
-BOARD_RAMDISK_OFFSET     := 0x01000000
-BOARD_TAGS_OFFSET        := 0x00000100
-BOARD_DTB_OFFSET         := 0x01f00000
-
-# Argümanları Tek Seferde ve Düzgün Tanımla
-BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE)
-BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --board "$(TARGET_BOOTLOADER_BOARD_NAME)"
-
 # System as root
-BOARD_ROOT_EXTRA_FOLDERS := dsp bt_firmware firmware persist
+# -----------------------------------------------------------------------------
+BOARD_ROOT_EXTRA_FOLDERS := bluetooth dsp firmware persist
 BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 
+# -----------------------------------------------------------------------------
 # Properties
+# -----------------------------------------------------------------------------
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# -----------------------------------------------------------------------------
 # Recovery
+# -----------------------------------------------------------------------------
+TARGET_NO_RECOVERY := true
 BOARD_USES_RECOVERY_AS_BOOT := true
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 BOARD_USES_METADATA_PARTITION := true
 
+# -----------------------------------------------------------------------------
 # AVB
+# -----------------------------------------------------------------------------
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_AVB_VBMETA_SYSTEM := system system_ext product
@@ -166,12 +177,11 @@ BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
 
-# Property desteği
-TW_INCLUDE_LIBRESETPROP := true
-
+# -----------------------------------------------------------------------------
 # Crypto
+# -----------------------------------------------------------------------------
 TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_CRYPTO_FBE := true
+#TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 TW_PREPARE_DATA_MEDIA_EARLY := true
@@ -182,24 +192,26 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
+# -----------------------------------------------------------------------------
 # TWRP Configuration
-TW_FRAMERATE := 120
-TW_EXCLUDE_APEX := true
+# -----------------------------------------------------------------------------
 TW_THEME := portrait_hdpi
-TW_SKIP_ADDITIONAL_FSTAB := true
+TW_MAX_BRIGHTNESS := 2047
+TW_DEFAULT_BRIGHTNESS := 1200
+#TW_SKIP_ADDITIONAL_FSTAB := true
 RECOVERY_SDCARD_ON_DATA := true
-TW_ENABLE_FS_COMPRESSION := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_INCLUDE_LPDUMP := true
+TW_ENABLE_FS_COMPRESSION := true
 TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := true
-TW_SCREEN_BLANK_ON_BOOT := true
+#TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_INCLUDE_RESETPROP := true
 #TW_HAS_EDL_MODE := true
 TW_INCLUDE_REPACKTOOLS := true
 TWRP_INCLUDE_LOGCAT := true
+TW_INCLUDE_LIBRESETPROP := true
 TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
@@ -213,7 +225,7 @@ TW_INCLUDE_FASTBOOTD := true
 # -----------------------------------------------------------------------------
 # Modules Load
 # -----------------------------------------------------------------------------
-TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/vendor/lib/modules)\")
+TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/prebuilt/modules)\")
 
 # -----------------------------------------------------------------------------
 # Override System Properties
